ead6d01b9c2d0af6c9eb5f7efa5610399ec1e1c1
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-05 09:23:13 +0100

    day 5 task 2

---
+++ advent/day_05/task_2.py
@@ -0,0 +1,27 @@
+import operator
+import re
+from pathlib import Path
+from typing import Iterator, NamedTuple
+
+from ..cli_utils import wrap_main
+from ..io_utils import get_stripped_lines
+from .task_1 import parse_instructions, parse_stacks
+
+
+@wrap_main
+def main(filename: Path) -> str:
+    input_lines = iter(get_stripped_lines(filename))
+    stacks = parse_stacks(input_lines)
+    for instruction in parse_instructions(input_lines):
+        source_stack = stacks[instruction.from_]
+        target_stack = stacks[instruction.to]
+        lifted: list[str] = []
+        for _ in range(instruction.count_):
+            lifted.append(source_stack.pop())
+        target_stack.extend(reversed(lifted))
+
+    return "".join(map(operator.itemgetter(-1), stacks))
+
+
+if __name__ == "__main__":
+    main()
