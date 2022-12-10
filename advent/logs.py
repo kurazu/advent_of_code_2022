0594b146300ee2e62d78ade7fd1d20f7ab7f61fd
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-10 11:32:45 +0100

    day 10 scaffolding

---
+++ advent/logs.py
@@ -0,0 +1,15 @@
+import logging
+
+from . import __name__ as package_name
+
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        level=logging.WARNING,
+        format="[%(asctime)s][%(levelname)8s][%(name)s] %(message)s",
+    )
+    for logger_name, level in {
+        package_name: logging.DEBUG,
+        __name__: logging.DEBUG,
+    }.items():
+        logging.getLogger(logger_name).setLevel(level)
