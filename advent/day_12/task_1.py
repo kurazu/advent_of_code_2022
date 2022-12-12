70d7b881c71cb0f242d55d0bb177f00d48388577
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-12 09:54:23 +0100

    day 12 task 1 callbacks

--- advent/day_12/task_1.py
+++ advent/day_12/task_1.py
@@ -1,7 +1,7 @@
 import logging
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable, NamedTuple
+from typing import Iterable, NamedTuple, Protocol
 
 import numpy as np
 import numpy.typing as npt
@@ -52,47 +52,62 @@ def argmin2d(array: npt.NDArray[np.uint32]) -> Position:
     return Position(y=y, x=x)
 
 
-def find_path(board: Board) -> int:
+class CanClimbCallback(Protocol):
+    def __call__(self, current_elevation: int, neighbor_elevation: int) -> bool:
+        ...
+
+
+class EarlyStoppingCallback(Protocol):
+    def __call__(self, neighbor_position: Position, neighbor_elevation: int) -> bool:
+        ...
+
+
+def find_path(
+    tiles: npt.NDArray[np.uint8],
+    *,
+    start_position: Position,
+    end_position: Position,
+    can_climb_callback: CanClimbCallback,
+    early_stopping_callback: EarlyStoppingCallback,
+) -> int:
     logger.debug(
         "Searching for best path between %s and %s",
-        board.start_position,
-        board.end_position,
+        start_position,
+        end_position,
     )
-    height, width = board.tiles.shape
-    visited = np.zeros_like(board.tiles, dtype=np.bool)
-    costs = np.full_like(
-        board.tiles, fill_value=np.iinfo(np.uint32).max, dtype=np.uint32
-    )
-    costs[board.start_position] = 0
+    height, width = tiles.shape
+    visited = np.zeros_like(tiles, dtype=bool)
+    costs = np.full_like(tiles, fill_value=np.iinfo(np.uint32).max, dtype=np.uint32)
+    costs[start_position] = 0
 
     def get_unvisited_neighbors(position: Position) -> Iterable[Position]:
-        current_elevation = board.tiles[position]
+        current_elevation = tiles[position]
         north = Position(y=position.y - 1, x=position.x)
         if (
             north.y >= 0
             and not visited[north]
-            and board.tiles[north] <= current_elevation + 1
+            and can_climb_callback(current_elevation, tiles[north])
         ):
             yield north
         south = Position(y=position.y + 1, x=position.x)
         if (
             south.y < height
             and not visited[south]
-            and board.tiles[south] <= current_elevation + 1
+            and can_climb_callback(current_elevation, tiles[south])
         ):
             yield south
         west = Position(y=position.y, x=position.x - 1)
         if (
             west.x >= 0
             and not visited[west]
-            and board.tiles[west] <= current_elevation + 1
+            and can_climb_callback(current_elevation, tiles[west])
         ):
             yield west
         east = Position(y=position.y, x=position.x + 1)
         if (
             east.x < width
             and not visited[east]
-            and board.tiles[east] <= current_elevation + 1
+            and can_climb_callback(current_elevation, tiles[east])
         ):
             yield east
 
@@ -102,23 +117,34 @@ def find_path(board: Board) -> int:
         lowest_cost = costs[lowest_cost_index]
         logger.debug("Visiting %s with cost %d", lowest_cost_index, lowest_cost)
         unvisited_neighbors = get_unvisited_neighbors(lowest_cost_index)
+        neighbor_cost: int = lowest_cost + 1
         for neighbor_idx in unvisited_neighbors:
-            costs[neighbor_idx] = lowest_cost + 1
-            logger.debug(
-                "Found neighbor %s with cost %d", neighbor_idx, lowest_cost + 1
-            )
-            if neighbor_idx == board.end_position:
-                break
+            costs[neighbor_idx] = neighbor_cost
+            logger.debug("Found neighbor %s with cost %d", neighbor_idx, neighbor_cost)
+            if early_stopping_callback(neighbor_idx, tiles[neighbor_idx]):
+                logger.debug("Early stopping at %s", neighbor_idx)
+                return neighbor_cost
         visited[lowest_cost_index] = True
 
-    best_cost: int = costs[board.end_position]
-    return best_cost
+    raise AssertionError("Algorithm should have stopped early")
 
 
 @wrap_main
 def main(filename: Path) -> str:
     board = parse_board(filename)
-    cost = find_path(board)
+    cost = find_path(
+        board.tiles,
+        start_position=board.start_position,
+        end_position=board.end_position,
+        can_climb_callback=(
+            lambda current_elevation, neighbor_elevation: neighbor_elevation
+            <= current_elevation + 1
+        ),
+        early_stopping_callback=(
+            lambda neighbor_position, neighbor_elevation: neighbor_position
+            == board.end_position
+        ),
+    )
     return str(cost)
 
 
