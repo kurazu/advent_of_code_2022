a15d624865fec6cb65fd3d561757fc38c816087e
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-17 19:14:22 +0100

    day 17 task 1 refactor: use ints instead of arrays

--- advent/day_17/task_1.py
+++ advent/day_17/task_1.py
@@ -5,9 +5,7 @@ import logging
 from pathlib import Path
 from typing import Iterator
 
-import numpy as np
 import tqdm
-from numpy import typing as npt
 
 from ..cli_utils import wrap_main
 from ..io_utils import get_stripped_lines
@@ -21,83 +19,81 @@ class Direction(str, enum.Enum):
 
 logger = logging.getLogger(__name__)
 
-ROCKS: list[npt.NDArray[np.bool_]] = [
-    np.array([[1, 1, 1, 1]], dtype=np.bool_),
-    np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=np.bool_),
-    np.array([[0, 0, 1], [0, 0, 1], [1, 1, 1]], dtype=np.bool_),
-    np.array([[1], [1], [1], [1]], dtype=np.bool_),
-    np.array([[1, 1], [1, 1]], dtype=np.bool_),
+ROCKS: list[list[int]] = [
+    [int("000111100", 2)],
+    [int("000010000", 2), int("000111000", 2), int("000010000", 2)],
+    [int("000001000", 2), int("000001000", 2), int("000111000", 2)],
+    [
+        int("000100000", 2),
+        int("000100000", 2),
+        int("000100000", 2),
+        int("000100000", 2),
+    ],
+    [int("000110000", 2), int("000110000", 2)],
 ]
 
 WIDTH = 7
-FLOOR = np.array([1] * WIDTH, dtype=np.bool_)
-TUNNEL = np.array([0] * WIDTH, dtype=np.bool_)
+FLOOR = int("111111111", 2)
+TUNNEL = int("100000001", 2)
+
+MASK = int("011111110", 2)
 
 
-def find_top_index(board: list[npt.NDArray[np.bool_]]) -> int:
+def find_top_index(board: list[int]) -> int:
     idx = len(board) - 1
     for row in board[::-1]:
-        if row.any():
+        if row & MASK:
             return idx
         idx -= 1
     raise AssertionError()
 
 
-def _visualize_board(board: list[npt.NDArray[np.bool_]]) -> str:
+def _visualize_board(board: list[int]) -> str:
     buf = io.StringIO()
     for row in board[::-1]:
-        buf.write("".join("#" if cell else "." for cell in row))
+        bits = bin(row)[2:].rjust(7, "0").replace("0", ".").replace("1", "#")
+        buf.write(bits)
         buf.write("\n")
     return buf.getvalue()
 
 
-def visualize_board(board: list[npt.NDArray[np.bool_]]) -> None:
+def visualize_board(board: list[int]) -> None:
     if logger.isEnabledFor(logging.DEBUG):
         logger.debug("Board:\n%s", _visualize_board(board))
 
 
 def clashes(
-    board: list[npt.NDArray[np.bool_]],
-    rock: npt.NDArray[np.bool_],
+    board: list[int],
+    rock: list[int],
     top: int,
-    left: int,
 ) -> bool:
-    rock_height, rock_width = rock.shape
-    if left < 0:
-        logger.debug("Rock clashes with left wall")
-        return True
-    if left + rock_width > WIDTH:
-        logger.debug("Rock clashes with right wall")
-        return True
     for idx, row in enumerate(rock):
-        if (board[top - idx][left : left + rock_width] & row).any():
-            logger.debug("Rock clashes with other rock")
+        if board[top - idx] & row:
             return True
-    logger.debug("Rock does not clash")
     return False
 
 
-def materialize_rock(
-    board: list[npt.NDArray[np.bool_]], rock: npt.NDArray[np.bool_], top: int, left: int
-) -> None:
-    rock_height, rock_width = rock.shape
+def materialize_rock(board: list[int], rock: list[int], top: int) -> None:
     for idx, row in enumerate(rock):
-        board[top - idx][left : left + rock_width] |= row
+        board[top - idx] |= row
 
 
-DIRECTION_TO_OFFSET = {
-    Direction.LEFT: -1,
-    Direction.RIGHT: 1,
-}
+# TODO add cache
+def move_rock(rock: list[int], direction: Direction) -> list[int]:
+    if direction == Direction.LEFT:
+        return [row << 1 for row in rock]
+    else:
+        assert direction == Direction.RIGHT
+        return [row >> 1 for row in rock]
 
 
 def simulate_step(
-    board: list[npt.NDArray[np.bool_]],
-    rock: npt.NDArray[np.bool_],
+    board: list[int],
+    rock: list[int],
     jet: Iterator[Direction],
 ) -> None:
     logger.debug("Simulating step of rock %s", rock)
-    rock_height, rock_width = rock.shape
+    rock_height = len(rock)
     # spawn new rock
     current_top_idx = find_top_index(board)
     logger.debug("Current top index: %s", current_top_idx)
@@ -107,31 +103,29 @@ def simulate_step(
     logger.debug("Needed spawn top index: %s", top_spawn_idx)
     # intitial spawn positions
     top = top_spawn_idx
-    left = 2
     # expand board if neecessary
     while len(board) <= top_spawn_idx:
         logger.debug("Adding new row to board")
-        board.append(np.copy(TUNNEL))
+        board.append(TUNNEL)
     # visualize_board(board)
 
     while True:
         direction = next(jet)
-        offset = DIRECTION_TO_OFFSET[direction]
-        if clashes(board, rock, top, left + offset):
+        moved_rock = move_rock(rock, direction)
+        if clashes(board, moved_rock, top):
             logger.debug("Jet stream cannot move rock")
         else:
             logger.debug("Jet stream moves rock %s", direction.name)
-            left += offset
+            rock = moved_rock
 
-        if clashes(board, rock, top - 1, left):
+        if clashes(board, rock, top - 1):
             logger.debug("Rock cannot fall anymore")
             break
         else:
             logger.debug("Rock falls")
             top -= 1
 
-    materialize_rock(board, rock, top, left)
-    visualize_board(board)
+    materialize_rock(board, rock, top)
 
 
 @wrap_main
@@ -142,16 +136,16 @@ def main(filename: Path) -> str:
     board = get_starting_board()
     for _ in tqdm.trange(steps):
         simulate_step(board, next(rocks), jet)
+        visualize_board(board)
     logger.info("Board after %s steps:\n%s", steps, _visualize_board(board))
     return str(find_top_index(board))
 
 
-def get_starting_board() -> list[npt.NDArray[np.bool_]]:
-    board: list[npt.NDArray[np.bool_]] = [FLOOR]
-    return board
+def get_starting_board() -> list[int]:
+    return [FLOOR]
 
 
-def get_rocks() -> Iterator[npt.NDArray[np.bool_]]:
+def get_rocks() -> Iterator[list[int]]:
     rocks = it.cycle(ROCKS)
     return rocks
 
