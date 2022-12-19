8ecf0769d9695467fc51f5d161419d5d34f6a1fd
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-19 11:59:08 +0100

    day 19 task 1 WIP

--- advent/day_19/task_1.py
+++ advent/day_19/task_1.py
@@ -2,9 +2,13 @@ import itertools as it
 import logging
 import operator
 import re
+from copy import copy
 from dataclasses import dataclass
 from pathlib import Path
 from typing import NewType
+
+import tqdm
+from returns.curry import partial
 
 from ..cli_utils import wrap_main
 from ..io_utils import get_stripped_lines
@@ -64,8 +68,127 @@ def parse_blueprints(filename: Path) -> dict[BluePrintId, BluePrint]:
     return blueprints
 
 
+@dataclass(unsafe_hash=True)
+class State:
+    ore: Ore = Ore(0)
+    clay: Clay = Clay(0)
+    obsidian: Obsidian = Obsidian(0)
+    geode: Geode = Geode(0)
+
+    ore_robots: int = 1
+    clay_robots: int = 0
+    obsidian_robots: int = 0
+    geode_robots: int = 0
+
+    time_left: int = 24
+
+
+def collect_resources(state: State) -> None:
+    state.ore = Ore(state.ore + state.ore_robots)
+    state.clay = Clay(state.clay + state.clay_robots)
+    state.obsidian = Obsidian(state.obsidian + state.obsidian_robots)
+    state.geode = Geode(state.geode + state.geode_robots)
+
+
+@dataclass
+class CacheStats:
+    hits: int = 0
+    misses: int = 0
+
+    @property
+    def total(self) -> int:
+        return self.hits + self.misses
+
+
+def _score_blueprint(
+    cache: dict[State, State],
+    blueprint: BluePrint,
+    state: State,
+) -> State:
+    if state in cache:
+        return cache[state]
+
+    next_state = copy(state)
+
+    # robots are harvesting resources
+    collect_resources(next_state)
+
+    # time is running out
+    next_state.time_left -= 1
+
+    if next_state.time_left == 0:
+        cache[state] = next_state
+        logger.debug("Reached possible end state with score %s", next_state.geode)
+        return next_state
+
+    # We have the following possibilities (exclusive):
+    possibilities: list[State] = []
+    # build a geode robot
+    geode_cost_ore, geode_cost_obsidian = blueprint.geode_robot_cost
+    if state.ore >= geode_cost_ore and state.obsidian >= geode_cost_obsidian:
+        build_geode_robot_possibility = copy(next_state)
+        build_geode_robot_possibility.ore = Ore(
+            build_geode_robot_possibility.ore - geode_cost_ore
+        )
+        build_geode_robot_possibility.obsidian = Obsidian(
+            build_geode_robot_possibility.obsidian - geode_cost_obsidian
+        )
+        build_geode_robot_possibility.geode_robots += 1
+        # return _score_blueprint(cache, blueprint, build_geode_robot_possibility)
+        possibilities.append(build_geode_robot_possibility)
+    # build an obsidian robot
+    obsidian_cost_ore, obsidian_cost_clay = blueprint.obsidian_robot_cost
+    if state.ore >= obsidian_cost_ore and state.clay >= obsidian_cost_clay:
+        build_obsidian_robot_possibility = copy(next_state)
+        build_obsidian_robot_possibility.ore = Ore(
+            build_obsidian_robot_possibility.ore - obsidian_cost_ore
+        )
+        build_obsidian_robot_possibility.clay = Clay(
+            build_obsidian_robot_possibility.clay - obsidian_cost_clay
+        )
+        build_obsidian_robot_possibility.obsidian_robots += 1
+        possibilities.append(build_obsidian_robot_possibility)
+        # return _score_blueprint(cache, blueprint, build_obsidian_robot_possibility)
+    # build a clay robot
+    if state.clay >= blueprint.clay_robot_cost:
+        build_clay_robot_possibility = copy(next_state)
+        build_clay_robot_possibility.clay = Clay(
+            build_clay_robot_possibility.clay - blueprint.clay_robot_cost
+        )
+        build_clay_robot_possibility.clay_robots += 1
+        possibilities.append(build_clay_robot_possibility)
+        # return _score_blueprint(cache, blueprint, build_clay_robot_possibility)
+    # build an ore robot
+    if state.ore >= blueprint.ore_robot_cost:
+        build_ore_robot_possibility = copy(next_state)
+        build_ore_robot_possibility.ore = Ore(
+            build_ore_robot_possibility.ore - blueprint.ore_robot_cost
+        )
+        build_ore_robot_possibility.ore_robots += 1
+        possibilities.append(build_ore_robot_possibility)
+        # return _score_blueprint(cache, blueprint, build_ore_robot_possibility)
+
+    # return _score_blueprint(cache, blueprint, next_state)
+    # wait
+    possibilities.append(next_state)
+
+    best_state = cache[state] = max(
+        map(partial(_score_blueprint, cache, blueprint), possibilities),
+        key=lambda s: s.geode,
+    )
+    return best_state
+
+
 def score_blueprint(blueprint: BluePrint) -> Geode:
-    return Geode(0)
+    cache: dict[State, Geode] = {}
+    state = State()
+    state.clay = Clay(state.clay + blueprint.clay_robot_cost)
+    state.time_left -= blueprint.clay_robot_cost
+
+    best_state = _score_blueprint(cache, blueprint, state)
+    best_score = best_state.geode
+    logger.info("Blueprint scored %d with %s", best_score, best_state)
+    return best_score
 
 
 @wrap_main
@@ -73,7 +196,7 @@ def main(filename: Path) -> str:
     blueprints = parse_blueprints(filename)
     scores: dict[BluePrintId, Geode] = {
         blueprint_id: score_blueprint(blueprint)
-        for blueprint_id, blueprint in blueprints.items()
+        for blueprint_id, blueprint in tqdm.tqdm(blueprints.items(), desc="Blueprints")
     }
     levels = it.starmap(operator.mul, scores.items())
     total = sum(levels)
@@ -81,5 +204,5 @@ def main(filename: Path) -> str:
 
 
 if __name__ == "__main__":
-    setup_logging()
+    setup_logging(logging.INFO)
     main()
