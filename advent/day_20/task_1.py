a75633790bff610d70d8ec994c330d82bb185971
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-20 08:07:57 +0100

    day 20 task 1 wip

--- advent/day_20/task_1.py
+++ advent/day_20/task_1.py
@@ -1,6 +1,11 @@
+import itertools as it
 import logging
 from pathlib import Path
 from typing import Iterable
+
+import more_itertools as mit
+import numpy as np
+from numpy import typing as npt
 
 from ..cli_utils import wrap_main
 from ..io_utils import get_stripped_lines
@@ -13,10 +18,24 @@ def read_numbers(filename: Path) -> Iterable[int]:
     return map(int, get_stripped_lines(filename))
 
 
+def mix(
+    numbers: npt.NDArray[np.int32], indices: npt.NDArray[np.uint32]
+) -> npt.NDArray[np.int32]:
+    return numbers
+
+
 @wrap_main
 def main(filename: Path) -> str:
-    numbers = list(read_numbers(filename))
+    numbers = np.array(list(read_numbers(filename)), dtype=np.int32)
+    indices = np.arange(len(numbers), dtype=np.uint32)
     logger.debug("Read %d numbers", len(numbers))
+    mixed = mix(numbers, indices)
+    zero_index: int
+    ((zero_index,),) = np.where(mixed == 0)
+    wrapped = it.cycle(it.chain(mixed[zero_index:], mixed[:zero_index]))
+    items = (mit.nth(wrapped, 1000) for _ in range(3))
+    for item in items:
+        logger.debug("Item: %d", item)
     return ""
 
 
