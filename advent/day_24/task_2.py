ccc87b017b654bdf8a1c67a660f4bc20deeb4ab8
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-25 10:54:01 +0100

    day 24 task 2 wip

--- advent/day_24/task_2.py
+++ advent/day_24/task_2.py
@@ -3,13 +3,29 @@ from pathlib import Path
 
 from ..cli_utils import wrap_main
 from ..logs import setup_logging
+from .task_1 import PositionInTime, find_min_distance, read_board
 
 logger = logging.getLogger(__name__)
 
 
 @wrap_main
 def main(filename: Path) -> str:
-    return ""
+    board = read_board(filename)
+    end_point = find_min_distance(
+        board,
+        start_point=PositionInTime(0, board.north_exit),
+        end_position=board.south_exit,
+    )
+    logger.info("Crosses the exit at %s", end_point)
+    end_point = find_min_distance(
+        board, start_point=end_point, end_position=board.north_exit
+    )
+    logger.info("Crosses the exit at %s", end_point)
+    end_point = find_min_distance(
+        board, start_point=end_point, end_position=board.south_exit
+    )
+    logger.info("Crosses the exit at %s", end_point)
+    return str(end_point.time)
 
 
 if __name__ == "__main__":
