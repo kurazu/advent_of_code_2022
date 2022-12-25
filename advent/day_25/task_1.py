610524d9369c766d652b0c8716b7dc520ee6c456
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-12-25 15:00:41 +0100

    day 25 task 1 wip

--- advent/day_25/task_1.py
+++ advent/day_25/task_1.py
@@ -38,14 +38,15 @@ def int_to_snafu(number: int) -> SNAFU:
     snafu_digits: list[str] = []
     carry = 0
     while number != 0:
-        remainder = number % 5 + carry
+        remainder = (number + carry) % 5
         if remainder in {0, 1, 2}:
             snafu_digits.append(str(remainder))
             carry = 0
         elif remainder == 3:
             snafu_digits.append("=")
             carry = 1
-        elif remainder == 4:
+        else:
+            assert remainder == 4
             snafu_digits.append("-")
             carry = 1
         number //= 5
@@ -54,7 +55,9 @@ def int_to_snafu(number: int) -> SNAFU:
     return SNAFU("".join(reversed(snafu_digits)))
 
 
+# 1 5 25
 TEST_SAMPLES: list[tuple[int, SNAFU]] = [
+    (0, SNAFU("0")),
     (1, SNAFU("1")),
     (2, SNAFU("2")),
     (3, SNAFU("1=")),
@@ -65,8 +68,46 @@ TEST_SAMPLES: list[tuple[int, SNAFU]] = [
     (8, SNAFU("2=")),
     (9, SNAFU("2-")),
     (10, SNAFU("20")),
+    (11, SNAFU("21")),
+    (12, SNAFU("22")),
+    (13, SNAFU("1==")),
+    (14, SNAFU("1=-")),
     (15, SNAFU("1=0")),
+    (16, SNAFU("1=1")),
+    (17, SNAFU("1=2")),
+    (18, SNAFU("1-=")),
+    (19, SNAFU("1--")),
     (20, SNAFU("1-0")),
+    (21, SNAFU("1-1")),
+    (22, SNAFU("1-2")),
+    (23, SNAFU("10=")),
+    (24, SNAFU("10-")),
+    (25, SNAFU("100")),
+    (26, SNAFU("101")),
+    (27, SNAFU("102")),
+    (28, SNAFU("11=")),
+    (29, SNAFU("11-")),
+    (30, SNAFU("110")),
+    (31, SNAFU("111")),
+    (32, SNAFU("112")),
+    (33, SNAFU("12=")),
+    (34, SNAFU("12-")),
+    (35, SNAFU("120")),
+    (36, SNAFU("121")),
+    (37, SNAFU("122")),
+    (38, SNAFU("2==")),
+    (39, SNAFU("2=-")),
+    (40, SNAFU("2=0")),
+    (41, SNAFU("2=1")),
+    (42, SNAFU("2=2")),
+    (43, SNAFU("2-=")),
+    (44, SNAFU("2--")),
+    (45, SNAFU("2-0")),
+    (46, SNAFU("2-1")),
+    (47, SNAFU("2-2")),
+    (48, SNAFU("20=")),
+    (49, SNAFU("20-")),
+    (50, SNAFU("200")),
     (2022, SNAFU("1=11-2")),
     (12345, SNAFU("1-0---0")),
     (314159265, SNAFU("1121-1110-1=0")),
